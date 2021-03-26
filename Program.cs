b9cd51e42b29ae4628e26240decbbcd028a84d7f
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 19:44:35 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public abstract class ClasseBase
+{
+    public virtual void Metodo()
+    {
+        Console.WriteLine("Método ClasseBase");
+    }
+    public abstract void MetodoAbstrato();
+}
+public class Derivada: ClasseBase
+{
+    public override void Metodo()
+    {
+        Console.WriteLine("Método Derivado");
+    }
+    public override void MetodoAbstrato()
+    {
+        Console.WriteLine("Metodo MetodoAbstrato");    
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Derivada b = new Derivada();
+        b.Metodo();
+        ClasseBase c = new Derivada();
+        c.Metodo();
+        c.MetodoAbstrato();
+        Console.Read();
+    }
+}
